e4c0e916a8f9ec56c33c05411cc7249ee15c1d2c
Lucas Souza <145349067+llslucas@users.noreply.github.com>
2025-10-15 20:39:12 -0300

    Project build

--- app/src/main/java/com/unifae/lucas/mangosapp/MainActivity.kt
+++ app/src/main/java/com/unifae/lucas/mangosapp/MainActivity.kt
@@ -4,11 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import com.unifae.lucas.mangosapp.ui.component.HeaderCard
+import com.unifae.lucas.mangosapp.ui.screen.DashboardScreen
 import com.unifae.lucas.mangosapp.ui.theme.MangosAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,9 +13,7 @@ class MainActivity : ComponentActivity() {
     enableEdgeToEdge()
     setContent {
       MangosAppTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-          HeaderCard(modifier = Modifier.padding(innerPadding)) {  }
-        }
+        DashboardScreen()
       }
     }
   }
