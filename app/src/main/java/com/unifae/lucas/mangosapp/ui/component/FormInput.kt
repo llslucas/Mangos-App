030e38f5322b68a8f82f662da2a8b78568e89eeb
Lucas Souza <145349067+llslucas@users.noreply.github.com>
2025-10-24 21:42:45 -0300

    Create component: `FormInput`

---
+++ app/src/main/java/com/unifae/lucas/mangosapp/ui/component/FormInput.kt
@@ -0,0 +1,76 @@
+package com.unifae.lucas.mangosapp.ui.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import com.unifae.lucas.mangosapp.ui.theme.MangosAppTheme
+import com.unifae.lucas.mangosapp.ui.theme.Primary
+import com.unifae.lucas.mangosapp.ui.theme.Typography
+
+@Composable
+fun FormInput(
+  modifier: Modifier = Modifier,
+  title: String,
+  placeholder: String,
+  value: String,
+  onChange: (text: String) -> Unit,
+) {
+  Column(
+    modifier = modifier,
+    Arrangement.spacedBy(MangosAppTheme.sizing.sm)
+  ) {
+    Text(
+      text = title,
+      style = Typography.headlineMedium,
+      color = MaterialTheme.colorScheme.onBackground
+    )
+    OutlinedTextField(
+      modifier = Modifier.fillMaxWidth(),
+      value = value,
+      onValueChange = onChange,
+      placeholder = {
+        Text(
+          text = placeholder,
+          style = Typography.titleMedium,
+          color = MaterialTheme.colorScheme.onSecondary
+        )
+      },
+      shape = CircleShape,
+      textStyle = Typography.titleMedium,
+      colors = OutlinedTextFieldDefaults.colors(
+        focusedBorderColor = Primary,
+        focusedContainerColor = MaterialTheme.colorScheme.surface,
+        focusedTextColor = MaterialTheme.colorScheme.onSurface,
+        unfocusedBorderColor = Color.Transparent,
+        unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+        unfocusedTextColor = MaterialTheme.colorScheme.onSurface
+      )
+    )
+  }
+}
+
+@PreviewLightDark
+@Composable
+private fun FormInputPreview() {
+  val (text, setText) = remember { mutableStateOf("") }
+
+  MangosAppTheme {
+    FormInput(
+      title = "Título",
+      placeholder = "Placeholder",
+      value = text,
+      onChange = setText
+    )
+  }
+}
