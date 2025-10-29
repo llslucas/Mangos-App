95b402fd72eec7bbb569ffb1412400c91e1efede
Lucas Souza <145349067+llslucas@users.noreply.github.com>
2025-10-29 19:42:20 -0300

    Create component: `SubHeader`

---
+++ app/src/main/java/com/unifae/lucas/mangosapp/ui/component/SubHeader.kt
@@ -0,0 +1,37 @@
+package com.unifae.lucas.mangosapp.ui.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import com.unifae.lucas.mangosapp.ui.theme.MangosAppTheme
+import com.unifae.lucas.mangosapp.ui.theme.Typography
+
+@Composable
+fun SubHeader(
+  modifier: Modifier = Modifier,
+  title: String
+) {
+  Column(
+    modifier = modifier.fillMaxWidth()
+  ) {
+    Text(
+      text = title,
+      style = Typography.titleSmall,
+      color = MaterialTheme.colorScheme.onBackground
+    )
+  }
+}
+
+@PreviewLightDark
+@Composable
+private fun SubHeaderPreview() {
+  MangosAppTheme {
+    SubHeader(
+      title = "Text de ajuda para a página",
+    )
+  }
+}
