6ce0b552a3ad79fbec43c4667fd543fee2c5698e
Lucas Souza <145349067+llslucas@users.noreply.github.com>
2025-10-29 19:41:58 -0300

    Create component: `CustomButton`

---
+++ app/src/main/java/com/unifae/lucas/mangosapp/ui/component/CustomButton.kt
@@ -0,0 +1,52 @@
+package com.unifae.lucas.mangosapp.ui.component
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.unifae.lucas.mangosapp.ui.theme.MangosAppTheme
+import com.unifae.lucas.mangosapp.ui.theme.Primary
+import com.unifae.lucas.mangosapp.ui.theme.Typography
+
+@Composable
+fun CustomButton(
+  modifier: Modifier = Modifier,
+  onClick: () -> Unit,
+  text: String,
+  bgColor: Color = Primary,
+  textColor: Color = MaterialTheme.colorScheme.onBackground,
+  ) {
+  Button(
+    modifier = modifier,
+    onClick = onClick,
+    colors = ButtonDefaults.buttonColors(
+      containerColor = bgColor,
+      contentColor = textColor,
+    ),
+    content = {
+      Text(
+        modifier = Modifier.padding(8.dp),
+        text = text,
+        style = Typography.bodyMedium
+      )
+    }
+  )
+}
+
+@PreviewLightDark
+@Composable
+private fun CustomButtonPreview() {
+  MangosAppTheme {
+    CustomButton(
+      text = "Botão",
+      onClick = {}
+    )
+  }
+}
