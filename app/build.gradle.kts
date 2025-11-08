f0cb4b53159fe355642896c76798dd8bb84122cd
Lucas Souza <145349067+llslucas@users.noreply.github.com>
2025-11-08 14:27:06 -0300

    Removed files from .gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
   implementation(libs.androidx.material3)
   implementation(libs.androidx.ui.text.google.fonts)
   implementation(libs.androidx.material.icons.extended)
+  implementation(libs.androidx.lifecycle.viewmodel.compose)
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.junit)
   androidTestImplementation(libs.androidx.espresso.core)
